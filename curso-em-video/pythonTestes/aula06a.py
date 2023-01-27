468870cd062aed6371ac5803cf9cb11d7e31bcef
sabrinacramos <scramos.dev@gmail.com>
2023-01-27 12:51:37 -0300

    atualizado

---
+++ curso-em-video/pythonTestes/aula06a.py
@@ -0,0 +1,35 @@
+# STRINGS
+# n1 = input('Digite um valor: ')
+# print(type(n1))
+
+# n2 = (input('Digite um valor: '))
+# print(type(n2))
+
+# CONCATENA OS VALORES POR QUE SÃO STRINGS ('str')
+# s = n1 + n2
+# print('A soma vale ', s)
+
+# ----------------------------------------------------
+
+# INTEIRO
+# n1 = int(input('Digite um valor: '))
+# print(type(n1))
+
+# n2 = int(input('Digite um valor: '))
+# print(type(n2))
+
+# SOMA OS VALORES POIS SÃO NUMEROS ('int')
+# s = n1 + n2 
+# print('A soma vale ', s)
+
+# ----------------------------------------------------
+
+n1 = int(input('Digite um valor: '))
+print(type(n1))
+
+n2 = int(input('Digite um valor: '))
+print(type(n2))
+
+# UTILIZANDO .format()
+s = n1 + n2
+print('A soma de {} e {} vale {}'.format(n1, n2, s))
