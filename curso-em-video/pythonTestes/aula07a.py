4668cf1e2e35b94e09eb214895522b4a4dfbc62a
sabrinacramos <scramos.dev@gmail.com>
2023-01-30 17:37:34 -0300

    Atualizado

---
+++ curso-em-video/pythonTestes/aula07a.py
@@ -0,0 +1,20 @@
+n1 = int(input('Um valor: '))
+n2 = int(input('Um valor: '))
+
+# soma
+s = n1 + n2
+# subtração
+su = n1 - n2
+# multiplicação
+m = n1 * n2
+# divisão
+d = n1 / n2
+# divisão inteira
+di = n1 // n2
+# potência
+p = n1 ** n2
+# resto da divisão
+rd = n1 % n2
+
+print(f'A soma é {s}, a subtração é {su}, o produto é {m} e a divisão é {d}')
+print(f'Divisão inteira é {di}, potência é {p} e resto da divisão {rd}')
---
+++ curso-em-video/pythonTestes/aula07b.py
@@ -0,0 +1,11 @@
+# DESAFIOS
+# 1 - FAÇA UM PROGRAMA QUE LEIA UM NÚMERO INTEIRO E MOSTRE NA TELA O SEU SUCESSOR E SEU ANTECESSOR
+n1 = int(input('digite um número inteiro: '))
+n2 = 1
+
+soma = n1 + n2
+sub = n1 - n2
+
+print(f'Sucessor {soma} \nAntesessor {sub}')
+
+# 2 - 
