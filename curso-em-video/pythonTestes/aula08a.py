376557891e426db22056172d554e3df45598a4a9
sabrinacramos <scramos.dev@gmail.com>
2023-02-03 17:37:34 -0300

    Atuazlizado

--- curso-em-video/pythonTestes/aula08a.py
+++ curso-em-video/pythonTestes/aula08a.py
@@ -1,4 +1,24 @@
-import math
+# --> IMPORT MATH - importa a biblioteca inteira
+
+# import math
+# num = int(input('Digite um número: '))
+# raiz = math.sqrt(num)
+
+# print(f'A raiz de {num} é igual a {raiz}')
+
+# ## PARA ARREDONDAR PARA CIMA USAR 'math.ceil()'
+# print(f'A raiz de {num} é igual a {math.ceil(raiz)}')
+
+# ## PARA ARREDONDAR PARA BAIXO USAR 'math.floor()'
+# print(f'A raiz de {num} é igual a {math.floor(raiz)}')
+
+# ------------------------------------------------------------
+
+# --> FROM MATH IMPORT - importa coisas especificas
+
+from math import sqrt, floor
+
 num = int(input('Digite um número: '))
-raiz = math.sqrt(num)
-print(f'A raiz de {num} é igual a {raiz}')
+raiz = sqrt(num)
+
+print(f'A raiz de {num} é igual a {floor(raiz)}')
